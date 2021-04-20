cb51b10aeeb171e13cbb80ee64a09124889e3b95
GhostXCode7 <81632892+ofc-ghostxcode7@users.noreply.github.com>
2021-04-20 21:13:05 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = '';
+var url = 'https://api-ghostxcode7.herokuapp.com';
 try {
-if(url == '') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api-ghostxcode7.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
